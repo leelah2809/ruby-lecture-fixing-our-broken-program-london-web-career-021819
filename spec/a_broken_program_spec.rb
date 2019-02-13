ec057a0d71c4e8974adf7b9b19dcad8a21e6f8b4
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-13 15:28:35 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'does not raise an error when loaded' do
     expect{
       load 'lib/a_broken_program.rb'
-    }.to not_to_raise_error
+    }.to_not raise_error
   end
 
 end
